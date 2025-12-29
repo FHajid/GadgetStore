b5da4bdd73db5186670052c6cde843cc8344e536
FHNHajid <farhan.hajid101@gmail.com>
2025-12-29 23:36:28 +0700

    daily login

--- app/store/page.tsx
+++ app/store/page.tsx
@@ -2,8 +2,11 @@ import React from 'react'
 
 export default function store() {
   return (
-    <div>
-      
+    <div >
+      <main style={{height:"100vh" , padding:"30px 60px"}}  >
+        <h1>Store Page</h1>
+
+      </main>
     </div>
   )
 }
